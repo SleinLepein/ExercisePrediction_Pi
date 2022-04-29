e5fa67c31620f2b9c92bebcc48a7a3213465e666
KmanKid <david.kuehnemann@web.de>
2022-04-29 12:21:33 +0200

    The prediction is now being published to the mqtt-broker, requirements.txt has been extended with paho-mqtt

--- src/sender/sender.py
+++ src/sender/sender.py
@@ -1,4 +1,18 @@
+import paho.mqtt.client as paho
+topic = "CloudPredict"
+broker = "192.168.0.100"
+port = 1883
+
+def on_publish(client,userdata,result):
+    print("Prediction send \n")
+    pass
+
+client= paho.Client("Prediction")
+client.on_publish = on_publish
+client.connect(broker,port) 
+
+
 def send_to_app(msg):
-    # placeholder, mqtt broker needs to be implemented
-    # sends message to app
+    client.publish(topic,msg)
+    print("Published in "+topic)
     print(msg)
